859e46f672cf2e2896b88406396d480b706db34c
phillaung <yellowpete.kog@gmail.com>
2023-12-20 23:46:59 -0800

    about page

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
