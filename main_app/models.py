ef3e7097618e081bb56f373889e989f75237ce95
phillaung <yellowpete.kog@gmail.com>
2023-12-21 19:10:19 -0800

    tweet model, and home page with user ---< Tweet relationship

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tweet(models.Model):
+    content = models.CharField()
+    image_url = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from .models import Tweet
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'home.html')
+    tweets = Tweet.objects.filter(user=request.user)
+    return render(request, 'home.html', { 'tweets': tweets })
 
 def about(request):
     return render(request, 'about.html')
