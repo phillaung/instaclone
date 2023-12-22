08eb715c4d3ee3e8e330140a383d398e190cb65b
phillaung <yellowpete.kog@gmail.com>
2023-12-21 19:21:28 -0800

    detailed tweet page with possible expansion with comments

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('tweets/<int:tweet_id>/', views.detail, name='detail')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,5 +7,10 @@ def home(request):
     tweets = Tweet.objects.filter(user=request.user)
     return render(request, 'home.html', { 'tweets': tweets })
 
+@login_required
+def detail(request, tweet_id):
+    tweet = Tweet.objects.get(id=tweet_id)
+    return render(request, 'detail.html', { 'tweet': tweet })
+
 def about(request):
     return render(request, 'about.html')
